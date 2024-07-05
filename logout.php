825e74a0e085039e9c3a2318d4d687a918d6345d
Ege AYTAÇ <egenull0@gmail.com>
2024-07-06 02:31:40 +0300

    Gereksiz dosyalar silindi

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-    setcookie("username",true,time()-3600);
-    setcookie("login",true,time()-3600);
-    header("location:deneme.php");
-?>
--- views/_title.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $ozet = count($kategoriler).' kategoride '.count(filmleriGetir()).'  film listelenmiştir';
-?>
-
-<h1 class="mb-4"><?php echo baslik?></h1>
-<p class="text-muted">
-    <?php echo $ozet?>
-</p>
